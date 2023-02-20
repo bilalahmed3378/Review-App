7e8c106eb3fc31da9844f605145c8316fbf09697
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-02-20 15:57:59 +0500

    Initial Commit

---
+++ Review App/Review_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Review_AppApp.swift
+//  Review App
+//
+//  Created by Bilal Ahmed on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Review_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
