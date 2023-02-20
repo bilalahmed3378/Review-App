56ed21c171750b96e69de02f68f57b309852ea4b
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-02-20 18:03:50 +0500

    ljkl

---
+++ Review App/Screens/LoginSwicher.swift
@@ -0,0 +1,31 @@
+//
+//  LoginSwicher.swift
+//  Review App
+//
+//  Created by Bilal Ahmed on 20/02/2023.
+//
+
+import SwiftUI
+
+struct LoginSwitcher: View {
+    @State var isUserLoggedIn : Bool = false
+    
+    @State var isLoginView : Bool = false
+
+
+    var body: some View {
+        ZStack{
+            if(self.isUserLoggedIn){
+                
+            }
+            else{
+                if(self.isLoginView){
+                    SignupScreen(pushToSignup: self.$isLoginView)
+                }
+                else{
+                    LoginScreen(pushToSignup: self.$isLoginView)
+                }
+            }
+        }
+    }
+}
